719a5feb7f23102299210fcfe84fb530a8984c97
Haris Begić <70474327+harisbegic2001@users.noreply.github.com>
2021-07-07 10:47:05 +0000

    Added some changes

--- Main.java
+++ Main.java
@@ -7,11 +7,15 @@ class Main {
     System.out.println("Unesite ime studenta: ");
     stu.setname(in.nextLine());
     System.out.println("Unesite prezime studenta: ");
-    stu.setlastName(in.nextline());
+    stu.setlastName(in.nextLine());
     System.out.println("Unesite godinu studenta: ");
     stu.setGradeYear(in.nextInt());
    System.out.println("Unesite Vaš balance studenta: ");
    stu.setbalance(in.nextInt());
-    
+    System.out.println("Unesite kurseve koje biste htjeli dodati")
+    do {
+        String a = in.nextLine();
+        
+    }
   }
 }
